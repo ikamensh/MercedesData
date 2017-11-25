c1057bb8ef4dc6f4270bed40a0644ef3f41ef6cd
ikamensh <ikamenshchikov@gmail.com>
2017-11-25 14:42:12 +0100

    path to data fixed

---
+++ main.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df_train = pd.read_csv("data/train.csv", sep=',')
+df_test = pd.read_csv("data/test.csv", sep=',')
--- preprocessing/x_train_preparer.py
+++ preprocessing/x_train_preparer.py
@@ -7,7 +7,7 @@ Created on Sat Nov 25 14:32:32 2017
 
 import pandas as pd
 
-df = pd.read_csv('C:/Users/Deike/Documents/Python Scripts/mercedes/train.csv',index_col='ID')
+df = pd.read_csv('../data/train.csv',index_col='ID')
 
 x_train = df.drop('y',axis=1)
 y_train = df.y
@@ -16,4 +16,6 @@ for item in x_train:
     if type(x_train[item].iloc[0]) == str:
         x_train[item] = x_train[item].astype('category')
         x_train[item] = x_train[item].cat.codes
+
+print(x_train)
 
