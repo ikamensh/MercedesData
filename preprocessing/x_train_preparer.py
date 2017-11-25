c3ac57de2b5cf1b0b8246722831f320a0ee9ab62
LinusJon <linusdeike@gmail.com>
2017-11-25 14:36:37 +0100

    preprocessing of x_train and y_train

---
+++ preprocessing/x_train_preparer.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 14:32:32 2017
+
+@author: Deike
+"""
+
+import pandas as pd
+
+df = pd.read_csv('C:/Users/Deike/Documents/Python Scripts/mercedes/train.csv',index_col='ID')
+
+x_train = df.drop('y',axis=1)
+y_train = df.y
+
+for item in x_train:
+    if type(x_train[item].iloc[0]) == str:
+        x_train[item] = x_train[item].astype('category')
+        x_train[item] = x_train[item].cat.codes
+
