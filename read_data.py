4eaa2fc4b8f97236cff3eba7cebf34564f664fab
ikamensh <ikamenshchikov@gmail.com>
2017-11-26 11:28:48 +0100

    generated features

--- read_data.py
+++ read_data.py
@@ -5,7 +5,7 @@ x_train = preprocess_x("data/train.csv")
 x_test = preprocess_x("data/test.csv")
 
 x_train, y_train = extract_y(x_train)
-
+print(x_train.tail())
 #visualize_data(x_train, y_train)
 
 from sklearn.model_selection import train_test_split
@@ -14,5 +14,15 @@ x_train_split, x_val, y_train_split, y_val = train_test_split(x_train, y_train, 
 
 import pandas as pd
 features_mlp_train = pd.read_csv("features/train_mlp_features0.csv")
+features_mlp_train.index = x_train.index
+
 features_mlp_test = pd.read_csv("features/test_mlp_features0.csv")
+features_mlp_test.index = x_test.index
+
+x_train_aug = pd.merge(x_train, features_mlp_train, left_index=True, right_index=True)
+x_train_aug = pd.merge(x_test, features_mlp_test, left_index=True, right_index=True)
+
+print(x_train_aug.tail())
+
+
 
