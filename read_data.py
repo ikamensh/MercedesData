ac7e6106f0e621f648ea9ea9227abf97bcafb830
LinusJon <linusdeike@gmail.com>
2017-11-26 12:17:50 +0100

    GradienBoosting with grid search

--- read_data.py
+++ read_data.py
@@ -1,6 +1,8 @@
 from preprocessing.x_train_preparer import preprocess_x, extract_y
 from utility.visualize import visualize_data
 
+_corr_criteria = 0.05
+
 x_train = preprocess_x("data/train.csv")
 x_test = preprocess_x("data/test.csv")
 
@@ -20,9 +22,19 @@ features_mlp_test = pd.read_csv("features/test_mlp_features0.csv")
 features_mlp_test.index = x_test.index
 
 x_train_aug = pd.merge(x_train, features_mlp_train, left_index=True, right_index=True)
-x_train_aug = pd.merge(x_test, features_mlp_test, left_index=True, right_index=True)
-
-print(x_train_aug.tail())
-
-
-
+x_test_aug = pd.merge(x_test, features_mlp_test, left_index=True, right_index=True)
+print('Column amount: {}'.format(len(x_train_aug.columns)))
+#Filter for non zero standard deviation
+x_train_aug_std = x_train_aug.std()
+x_train_aug_std_fil = x_train_aug_std[x_train_aug_std != 0.0]
+x_train_aug = x_train_aug.filter(x_train_aug_std_fil.index.values)
+x_test_aug = x_test_aug.filter(x_train_aug_std_fil.index.values)
+print('Column amount: {}'.format(len(x_train_aug.columns)))
+#Filter for correlation to label
+ds_corr = x_train_aug.corrwith(y_train)
+ds_corr = abs(ds_corr)
+#apply filter
+ds_corr_fil = ds_corr[ds_corr > _corr_criteria]
+x_train_aug = x_train_aug.filter(ds_corr_fil.index.values)
+x_test_aug = x_test_aug.filter(ds_corr_fil.index.values)
+print('Column amount: {}'.format(len(x_train_aug.columns)))
